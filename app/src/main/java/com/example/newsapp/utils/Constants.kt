e8af0b89ff70cdadb2976ff86204a1c8bb573d47
Damir Galiakhmetov <damir11234@mail.ru>
2023-03-14 12:32:57 +0300

    lesson-3

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.newsapp.utils
+
+class Constants {
+
+    companion object{
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "45f015a6bce541b99f5a398c184620e0"
+    }
+}
